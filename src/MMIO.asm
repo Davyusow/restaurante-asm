497563407460814b4af42d80c610adb70e57d9b7
LUCAS CARVALHO DO ESPIRITO SANTO <lucas.espiritosanto@ufrpe.br>
2026-04-26 21:40:35 -0300

    Implementação final do MMIO
    - Implementei o MMIO

--- src/MMIO.asm
+++ src/MMIO.asm
@@ -1,21 +1,23 @@
 #Implementação inicial do MMIO (Memory-mapped Input/Output)
 
 .text
+# $t0 = Iterador do endereço
+# $t1 = Receiver Control; endereço = 0xFFFF0000
+# $t2 = Receiver Data = 0xFFFF0004
+# $t3 = Transmitter Control = 0xFFFF0008
+#  	Transmitter Data = 0xFFFF000c
 
-# $t0 = Receiver Control; endereço = 0xFFFF0000
-# $t1 = Receiver Data = 0xFFFF0004
-# $t2 = Transmitter Control = 0xFFFF0008
-# $t3 = Transmitter Data = 0xFFFF000c
+li $t0, 0xFFFF0000
 
 espera_Receiver_disponivel: # (0xFFFF0000, último byte == _0). Enquanto o último byte desse registrador for diferente de zero, ou seja não houver tecla digitada, volte no loop e esteja sempre disponível para receber, abordagem polling   
-	lw $t0, (0xFFFF0000) # Recebe o valor do Receiver Control
-	beq $t0, $zero, espera_Receiver_disponivel # Se zero, continua esperando
+	lw $t1, ($t0) # Recebe o valor do Receiver Control
+	beq $t1, $zero, espera_Receiver_disponivel # Se zero, continua esperando
 	
-lw $t1, 4(0xFFFF0000) # Recebe o byte transferido (caracter) do Receiver Data, que possui endereço 0xFFFF0004, vizinho ao Receiver Control
+lw $t2, 4($t0) # Recebe o byte transferido (caracter) do Receiver Data, que possui endereço 0xFFFF0004, vizinho ao Receiver Control
 
 espera_Transmitter_disponivel: # (0xFFFF0008, ultimo byte == _0) Enquanto o ultimo byte for zero, ou seja, não estiver disponivel, tente novamente, até estar
-	lw $t2, 8(0xFFFF0000) # Recebe o valor do Transmitter Control
-	beq $t0, $zero, espera_Transmitter_disponivel # Se zero continua esperando
+	lw $t3, 8($t0) # Recebe o valor do Transmitter Control
+	beq $t3, $zero, espera_Transmitter_disponivel # Se zero continua esperando
 	
-sw $t1, 12(0xFFFF0000)
+sw $t2, 12($t0)
 j espera_Receiver_disponivel
