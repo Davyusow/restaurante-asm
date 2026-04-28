978bd20eb088c201c4bd1b126813374e7cc5fad3
Davyusow <dayvsonf27@gmail.com>
2026-04-28 16:27:21 -0300

    Re-estruturação dos arquivos - 1 função por arquivo

--- src/strcmp.asm
+++ src/strcmp.asm
@@ -1,37 +1,10 @@
-# Diretório de funções gerais do projeto
-.data
-        pula_linha:     .asciiz "\n"
-
-.macro endl
-        la      $a0, pula_linha
-        li      $v0, 4
-        syscall
-.end_macro
-
-.text
-.globl print    # Expõe a label print para os arquivos do mesmo diretório
-.globl print_int
-.globl strcmp
-
-# Precisa alocar no $a0 o valor antes de imprimir!
-# Mostra uma string no terminal
-print:
-        li      $v0, 4  # syscall para imprimir a string
-        syscall         # Chama o syscall
-        endl
-        jr      $ra     # Retorna o ponteiro da função
-
-# Mostra um inteiro no terminal
-print_int:
-        li      $v0, 1
-        syscall
-        endl
-        jr      $ra
-
 # Compara duas strings
 # Retorna positivo caso seja uma letra maior
 # Retorna 0 se for igual
 # Retorna negativo se for uma letra menor
+.text
+.globl strcmp
+
 strcmp:
 loop:
         lb     $t0, 0($a0) # Carrega o primeiro bit da primeira string
