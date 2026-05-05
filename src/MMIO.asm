6f30b45e5571aed2a6823c8529db41c9b04f4999
LUCAS CARVALHO DO ESPIRITO SANTO <lucas.espiritosanto@ufrpe.br>
2026-05-05 12:23:37 -0300

    Desenvolvimento do MMIO
    + Adicionei buffer
    + Verificação de máximo de caracteres do buffer
    + Adicionei verificação do /n
    + Adicionei o /0 ao fim de todas as Strings após a leitura pelo buffer
    
    - Falta implementar a comparação da String com as funções predefinidas por nós, isso usará a função strcomp

--- src/MMIO.asm
+++ src/MMIO.asm
@@ -1,23 +1,54 @@
 #Implementação inicial do MMIO (Memory-mapped Input/Output)
 
+.data
+buffer: .space 51
+
 .text
-# $t0 = Iterador do endereço
+# OBS: Todos os registradores do tipo $t_ utilizados recebem o valor na memória, não o endereço
+
+
+# $t0 = valor do \n em ascii
+
+# $s0 = Iterador do endereço, tem como valor o próprio endereço do Receiver Control
 # $t1 = Receiver Control; endereço = 0xFFFF0000
 # $t2 = Receiver Data = 0xFFFF0004
 # $t3 = Transmitter Control = 0xFFFF0008
 #  	Transmitter Data = 0xFFFF000c
 
-li $t0, 0xFFFF0000
+# Endereços do buffer
+# $s1 = endereço base do buffer de String
+# $t4 = indice do array do buffer de String
 
-espera_Receiver_disponivel: # (0xFFFF0000, último byte == _0). Enquanto o último byte desse registrador for diferente de zero, ou seja não houver tecla digitada, volte no loop e esteja sempre disponível para receber, abordagem polling   
-	lw $t1, ($t0) # Recebe o valor do Receiver Control
+
+li $s0, 0xFFFF0000 # Recebe o endereço do Receiver Control, que sera usado como base para acessar os outros registradores
+la $s1, buffer # Endereço base do array do Buffer
+add $t4, $s1, $zero # Armazena o indice zero do array buffer
+addi $t0, $zero, '\n'
+
+
+espera_Receiver_disponivel: # (0xFFFF0000, último bit == _0). Enquanto o último bit desse registrador for diferente de zero, ou seja não houver tecla digitada, volte no loop e esteja sempre disponível para receber, abordagem polling   
+	lw $t1, ($s0) # Recebe o valor do Receiver Control
 	beq $t1, $zero, espera_Receiver_disponivel # Se zero, continua esperando
 	
-lw $t2, 4($t0) # Recebe o byte transferido (caracter) do Receiver Data, que possui endereço 0xFFFF0004, vizinho ao Receiver Control
+lw $t2, 4($s0) # Recebe o byte transferido (caracter) do Receiver Data, que possui endereço 0xFFFF0004, vizinho ao Receiver Control
 
-espera_Transmitter_disponivel: # (0xFFFF0008, ultimo byte == _0) Enquanto o ultimo byte for zero, ou seja, não estiver disponivel, tente novamente, até estar
-	lw $t3, 8($t0) # Recebe o valor do Transmitter Control
-	beq $t3, $zero, espera_Transmitter_disponivel # Se zero continua esperando
-	
-sw $t2, 12($t0)
+	 
+
+# Checagem do limite do buffer para não dar overflow
+# $t5 = Quantidade de caracteres já escritos
+# $t6 = Quantidade máxima de caracteres
+
+sub $t5, $t4, $s1 # Subtrai do endereço atual o valor do endereço inicial, assim, se por exemplo, eu estiver no indice 30 do Array, seria tipo: 1030 (endereço atual) - 1000 (endereço base), ou seja, já escrevi 30 caracteres, e armazeno isso em $t5
+li $t6, 50 # Defino que o tamanho máximo é 50
+
+beq $t5, $t6, enter_funcao # Caso tenha escrito já 50 caracteres, não armazeno mais, para não dar overflow, e parto para o enter_funcao
+
+sb $t2, 0($t4) # Armazena no buffer
+addi $t4, $t4, 1 # Itero o valor do array e desloco (como se fosse um Buffer[i++])
+
 j espera_Receiver_disponivel
+
+enter_funcao:
+sb $zero, 1($t4) #Adiciono um /0 no fim da string para não haver erro algum
+
+# Aqui deve-se fazer vários if's aninhados checando se o valor da string (buffer) é igual ao valor de algum comando já predefinido
