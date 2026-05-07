205d65397892e98d32aee87ddbcdcd8555fb396f
LUCAS CARVALHO DO ESPIRITO SANTO <lucas.espiritosanto@ufrpe.br>
2026-05-07 14:35:26 -0300

    Finalização MMIO
    Adicionei o switch case para as funções
    Enderecei com jal para as funções na main
    
    Fiz parte do loop inicial na main
    
    -Falta endereçar as 3 funções finais
    -Gerenciamento de memoria
    -Finalização de parte do loop na main
    -Ajustes finos

--- src/MMIO.asm
+++ src/MMIO.asm
@@ -1,6 +1,5 @@
 
 .data
-str_cmd_invalido: .asciiz "Comando invalido\n"
 str_cmd_cardapio_ad: .asciiz "cardapio_ad"
 cmd_cardapio_ad_sucesso: .asciiz "Item adicionado com sucesso"
 cmd_cardapio_ad_erro: .asciiz "Falha: número de item já cadastrado"
@@ -22,6 +21,38 @@ cmd_mesa_iniciar_inexistente: .asciiz "Falha: mesa inexistente"
 str_cmd_mesa_ad_item: .asciiz "mesa_ad_item"
 cmd_mesa_ad_item_sucesso: .asciiz "Item adicionado com sucesso"
 cmd_mesa_ad_item_invalido: .asciiz "Falha: código de item inválido"
+
+str_cmd_mesa_rm_item: .asciiz "mesa_rm_item"
+cmd_mesa_rm_item_sucesso: .asciiz "Item removido com sucesso"
+cmd_mesa_rm_item_inexistente_mesa: .asciiz "Falha: mesa inexistente"
+cmd_mesa_rm_item_desocupada: .asciiz "Falha: mesa nao iniciou atendimento"
+cmd_mesa_rm_item_inexistente_item: .asciiz "Falha: item nao consta na conta"
+cmd_mesa_rm_item_invalido_item: .asciiz "Falha: codigo do item invalido"
+
+
+str_cmd_mesa_format: .asciiz "mesa_format"
+cmd_mesa_format_sucesso: .asciiz "Mesa formatada com sucesso"
+
+str_cmd_mesa_parcial: .asciiz "mesa_parcial"
+
+
+str_cmd_mesa_pagar: .asciiz "mesa_pagar"
+
+
+str_cmd_mesa_fechar: .asciiz "mesa_fechar"
+
+
+str_cmd_salvar: .asciiz "salvar"
+
+
+str_cmd_recarregar: .asciiz "recarregar"
+
+
+str_cmd_formatar: .asciiz "formatar"
+
+
+
+str_cmd_invalido: .asciiz "Comando invalido\n"
 
 .text
 # OBS: Todos os registradores do tipo $t_ utilizados recebem o VALOR na memoria, nao o endereco
@@ -146,10 +177,23 @@ mmio_dividir_token:
 .globl apin # ascii p inteiro [apin] (reg $a0 numero). Funcao que converte um char em int
 
 	apin:
-	sb $t1, 0($a0) #adiciono o numero 
+	move $t2, $a0 # $t2 sera o iterador do array, assim recebe o ENDERECO do indice 0
 	addi $t0, $zero, '0' # Passa o valor ascii de '0' para $t0
-	sub $v0, $a0, $t0 # Subtrai do char o valor ascii de '0', assim, por exemplo, se for passado '3', sera feito '3' - '0', ou seja, 51 - 48 = 3, e retorna isso em $v0
-	jr $ra # Volta para o lugar onde a funcao foi chamada
+	move $v0, $zero # Limpo qualquer lixo que possa estar em $v0
+
+	apin_loop:
+	lb $t1, 0($t2) # $t1 recebe VALOR do indice $t2 da string numerica do array 
+
+	beq $t1, $zero, char_nulo # Caso seja o fim da string (\0) finaliza a funcao apin. Nao dara problema pois ascii '0' != 0
+
+	sub $t1, $t1, $t0 # Subtrai do char o valor ascii de '0', assim, por exemplo, se for passado '3', sera feito '3' - '0', ou seja, 51 - 48 = 3, e retorna isso em $v0
+
+	mul $v0, $v0, 10 # Vou receber o caractere transformado anteriormente e multiplicar por 10 para estar de acordo com as casas decimais. Ou seja, se recebi agora 1, e tinha recebido antes 2, logo sei que é o número 21, assim multiplicarei o 2 por 10
+	add $v0, $v0, $t1 # Somo as casas para ficar em uma unica string binaria (um unico numero) 
+
+	addi $t2, $t2, 1 # Itero para percorrer o array numerico (como se fosse um A[i++])
+
+	j apin_loop
 
 	char_nulo:
 	
@@ -158,13 +202,274 @@ mmio_dividir_token:
 .globl checar_comando # checar_comando (reg $a0 string). Funcao que checa se a string do buffer eh igual a algum comando predefinido
 
 	checar_comando:
+
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+
 	jal mmio_dividir_token # Chama a funcao de divisao de token para dividir o comando do seu argumento
 
 	move $s0, $v0 # Armazena em $s0 o token da funcao, o nome
 	move $s1, $v1 # Armazena em $s1 os argumentos da funcao
 
 	#Vou chamar a strcmp agora
-	move $a0, $s0
+	move $a0, $s0 # Passa o token do comando para $a0
+	
+	la $a1, str_cmd_cardapio_ad # Passa a string do comando "cardapio_ad" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_cardapio_ad # Se a string for igual, execute o comando ### Essa funcao tem parametro numerico
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_cardapio_rm # Passa a string do comando "cardapio_rm" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_cardapio_rm # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0 
+
+	la $a1, str_cmd_cardapio_list # Passa a string do comando "cardapio_list" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_cardapio_list # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_cardapio_format # Passa a string do comando "cardapio_format" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_cardapio_format # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_iniciar # Passa a string do comando "mesa_iniciar" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_iniciar # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_ad_item # Passa a string do comando "mesa_ad_item" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_ad_item # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_rm_item # Passa a string do comando "mesa_rm_item" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_rm_item # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_format # Passa a string do comando "mesa_format" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_format # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_parcial # Passa a string do comando "mesa_parcial" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_parcial # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_pagar # Passa a string do comando "mesa_pagar" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_pagar # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_mesa_fechar # Passa a string do comando "mesa_fechar" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_mesa_fechar # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_salvar # Passa a string do comando "salvar" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_salvar # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_recarregar # Passa a string do comando "recarregar" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_recarregar # Se a string for igual, execute o comando
+
+	move $a0, $s0 # Passa o token do comando para $a0
+
+	la $a1, str_cmd_formatar # Passa a string do comando "formatar" para $a1
+	jal strcmp
+	beq $v0, $zero, cmd_formatar # Se a string for igual, execute o comando
+	
+	j cmd_invalido # Se chegou aqui, o comando eh invalido
 
 
+	cmd_cardapio_ad: # 3 argumentoss <option1>-<option2>-<option3>
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal mmio_dividir_token # Vai dividir os argumentos da funcao
+	move $a0, $v0 # Passo como argumento em $a0 a primeira parte do token <option1>
+	jal apin # Transformo esse token em inteiro
+	
+	move $t0, $v0 # Escreve em $t0 o primeiro argumento
+
+	# A partir daqui o $v1 foi reescrito, assim esta <option2>-<option3>
+
+	move $a0, $v1 # Repete o processo para separar o resto dos tokens
+	jal mmio_dividir_token # Divide <option2> de <option3>
+	move $a0, $v0 # Passo <option2> para $a0
+	jal apin # Transformo esse token em inteiro
+	move $t1, $v0 # Escreve em $t1 o segundo argumento
+
+	move $a0, $v1 # Passo <option3> para $a0
+	jal apin # Transformo esse token em inteiro
+	move $t2, $v0 # Escreve em $t2 o terceiro argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+	move $a1, $t1
+	move $a2, $t2
+	
+	jal adicionar_item_cardapio #chamo a funcao
+
+	cmd_cardapio_rm: # 1 argumento <option1>
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal apin # Transformo esse token em inteiro
+	move $t0, $v0 # Escreve em $t0 o argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+
+	jal remover_item_cardapio
+
+
+	cmd_cardapio_list: # Sem argumentos
+
+	jal listar_cardapio 
+
+	cmd_cardapio_format: # Sem argumentos
+
+	jal formatar_cardapio
+
+	cmd_mesa_iniciar: # 3 argumentos <option1>-<option2>-<option3>
+
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal mmio_dividir_token # Vai dividir os argumentos da funcao
+	move $a0, $v0 # Passo como argumento em $a0 a primeira parte do token <option1>
+	jal apin # Transformo esse token em inteiro
+	
+	move $t0, $v0 # Escreve em $t0 o primeiro argumento
+
+	# A partir daqui o $v1 foi reescrito, assim esta <option2>-<option3>
+
+	move $a0, $v1 # Repete o processo para separar o resto dos tokens
+	jal mmio_dividir_token # Divide <option2> de <option3>
+	move $a0, $v0 # Passo <option2> para $a0
+	jal apin # Transformo esse token em inteiro
+	move $t1, $v0 # Escreve em $t1 o segundo argumento
+
+	move $a0, $v1 # Passo <option3> para $a0
+	jal apin # Transformo esse token em inteiro
+	move $t2, $v0 # Escreve em $t2 o terceiro argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+	move $a1, $t1
+	move $a2, $t2
+
+	jal iniciar_mesa
+
+
+	cmd_mesa_ad_item: # 2 argumentos <option1>-<option2>
+
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal mmio_dividir_token # Vai dividir os argumentos da funcao
+	move $a0, $v0 # Passo como argumento em $a0 a primeira parte do token <option1>
+	jal apin # Transformo esse token em inteiro
+	move $t0, $v0 # Escreve em $t0 o primeiro argumento
+
+	move $a0, $v1 # Passo como argumento em $a0 a segunda parte <option 2>
+	jal apin # Transformo esse token em inteiro
+	move $t1, $v0 # Escreve em $t1 o segundo argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+	move $a1, $t1
+
+	jal mesa_ad_item
+
+	cmd_mesa_rm_item: # 2 argumentos <option1>-<option2>
+
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal mmio_dividir_token # Vai dividir os argumentos da funcao
+	move $a0, $v0 # Passo como argumento em $a0 a primeira parte do token <option1>
+	jal apin # Transformo esse token em inteiro
+	move $t0, $v0 # Escreve em $t0 o primeiro argumento
+
+	move $a0, $v1 # Passo como argumento em $a0 a segunda parte <option 2>
+	jal apin # Transformo esse token em inteiro
+	move $t1, $v0 # Escreve em $t1 o segundo argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+	move $a1, $t1
+
+	jal mesa_rm_item
+
+	cmd_mesa_format: # Sem argumentos
+
+	jal formatar_mesas
+
+	cmd_mesa_parcial: # 1 argumento <option1>
+
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal apin # Transformo esse token em inteiro
+	move $t0, $v0 # Escreve em $t0 o argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+
+	jal mesa_parcial
+
+	cmd_mesa_pagar: # 2 argumentos <option1>-<option2> 
+
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal mmio_dividir_token # Vai dividir os argumentos da funcao
+	move $a0, $v0 # Passo como argumento em $a0 a primeira parte do token <option1>
+	jal apin # Transformo esse token em inteiro
+	move $t0, $v0 # Escreve em $t0 o primeiro argumento
+
+	move $a0, $v1 # Passo como argumento em $a0 a segunda parte <option 2>
+	jal apin # Transformo esse token em inteiro
+	move $t1, $v0 # Escreve em $t1 o segundo argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+	move $a1, $t1
+
+	jal mesa_pagar
+
+	cmd_mesa_fechar: # 1 argumento <option1>
+
+	move $a0, $v1 # Passo como argumento o resto, ou seja, os argumentos da funcao
+	jal apin # Transformo esse token em inteiro
+	move $t0, $v0 # Escreve em $t0 o argumento
+
+	#Passagem dos argumentos para os devidos registradores
+	move $a0, $t0
+
+	mesa_fechar
+
+	cmd_salvar: # Sem argumentos
+
+	jal
+
+	cmd_recarregar: # Sem argumentos
+
+	jal
+
+	cmd_formatar: # Sem argumentos
+
+	jal
+
+
+	cmd_invalido:
+	la $a0, str_cmd_invalido
+	jal mmio_imprimir_tela
+
+	jr $ra
 		
